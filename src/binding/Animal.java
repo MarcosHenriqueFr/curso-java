9df1142dd22c2858f31f271a3b44b2bf362b65a3
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-03-24 11:13:48 -0300

    Alteracoes

--- src/binding/Animal.java
+++ src/binding/Animal.java
@@ -7,7 +7,7 @@ public class Animal {
         setNome(nome);
     }
 
-    //Sobrecarga estatica de metodos
+    //Sobrecarga estatica de metodos, compile time
     public void fazerBarulho(String som){
         System.out.println(som);
     }
@@ -16,6 +16,14 @@ public class Animal {
         for(int i = 0; i < repetir; i++){
             System.out.println(som);
         }
+    }
+
+    public static void pular(Animal a){
+        System.out.println("O Animal pulou! " + a.getNome());
+    }
+
+    public static void pular(Cachorro c) {
+        System.out.println("O cachorro pulou! " + c.getNome());
     }
 
     public String getNome() {
--- src/binding/TesteBinding.java
+++ src/binding/TesteBinding.java
@@ -6,12 +6,17 @@ public class TesteBinding {
         Animal a1 = new Cachorro();
         Animal a2 = new Animal("Pássaro");
 
+        //Runtime
         Animal.AtividadeAnimal.comer(a1); //passo o cachorro dinamicamente
         Animal.AtividadeAnimal.comer(a2);
 
         //uso de estaticos, poderia ter colocado os valores de som dentro da minha classe animal ou cachorro
         a1.fazerBarulho("Au Au");
         a2.fazerBarulho("Piu piu", 3);
+
+        //Faz uma escolha estática e pula sozinho - no tempo do compilador
+        Animal.pular(a1);
+        Animal.pular(a2);
 
         System.out.println(a1.getNome());
         System.out.println(a2.getNome());
